83254cb19b06ef26551e4887c34cc940ab9e9c00
Will Harrison <willharrison2016@gmail.com>
2024-08-26 10:32:26 -0500

    Update PreferencesDlg.cpp

--- CommandAliasManager/src/PreferencesDlg.cpp
+++ CommandAliasManager/src/PreferencesDlg.cpp
@@ -1,1 +1,33 @@
-create preferences 
+#include "PreferencesDlg.h"
+#include <afxdlgs.h>
+
+BEGIN_MESSAGE_MAP(PreferencesDlg, CDialogEx)
+    ON_BN_CLICKED(IDC_SAVE_BUTTON, &PreferencesDlg::OnSavePreferences)
+END_MESSAGE_MAP()
+
+PreferencesDlg::PreferencesDlg(CWnd* pParent /*=nullptr*/)
+    : CDialogEx(IDD_PREFERENCESDIALOG, pParent)
+{
+}
+
+void PreferencesDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CDialogEx::DoDataExchange(pDX);
+}
+
+BOOL PreferencesDlg::OnInitDialog()
+{
+    CDialogEx::OnInitDialog();
+    LoadPreferences();
+    return TRUE;
+}
+
+void PreferencesDlg::OnSavePreferences()
+{
+    // Logic to save user preferences
+}
+
+void PreferencesDlg::LoadPreferences()
+{
+    // Logic to load user preferences
+}

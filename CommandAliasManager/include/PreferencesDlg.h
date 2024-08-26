edd86522696ebac9d69852ed25750588ed50789d
Will Harrison <willharrison2016@gmail.com>
2024-08-26 11:45:08 -0500

    Update PreferencesDlg.h

--- CommandAliasManager/include/PreferencesDlg.h
+++ CommandAliasManager/include/PreferencesDlg.h
@@ -1,1 +1,23 @@
-create preferences header
+#pragma once
+#include <afxwin.h>
+
+class CPreferencesDlg : public CDialogEx {
+public:
+    CPreferencesDlg(CWnd* pParent = nullptr);
+    enum { IDD = IDD_PREFERENCES_DIALOG };
+
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+    virtual BOOL OnInitDialog();
+
+private:
+    int m_numRecentFiles;
+    CString m_defaultFileTypeFilter;
+
+    void LoadPreferences();
+    void SavePreferences();
+
+    afx_msg void OnOK();
+
+    DECLARE_MESSAGE_MAP()
+};

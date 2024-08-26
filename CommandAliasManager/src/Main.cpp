94acc689f4785a27cf68986be9b6f2a002b9cf40
Will Harrison <willharrison2016@gmail.com>
2024-08-26 10:30:43 -0500

    Update Main.cpp

--- CommandAliasManager/src/Main.cpp
+++ CommandAliasManager/src/Main.cpp
@@ -1,1 +1,19 @@
-create src directory
+#include "MainDlg.h"
+#include <afxwin.h>
+
+class CommandAliasManagerApp : public CWinApp
+{
+public:
+    virtual BOOL InitInstance();
+};
+
+BOOL CommandAliasManagerApp::InitInstance()
+{
+    CWinApp::InitInstance();
+    MainDlg dlg;
+    m_pMainWnd = &dlg;
+    dlg.DoModal();
+    return FALSE;
+}
+
+CommandAliasManagerApp theApp;

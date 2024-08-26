85aea867f7dda855349575461758f59c7bf04fd8
Will Harrison <willharrison2016@gmail.com>
2024-08-26 09:57:12 -0500

    Update MainDlg.h

--- CommandAliasManager/include/MainDlg.h
+++ CommandAliasManager/include/MainDlg.h
@@ -1,1 +1,27 @@
-create include directory 
+#pragma once
+#include "afxwin.h"
+#include "FileBrowserDlg.h"
+#include "PreferencesDlg.h"
+#include "CommandAliasDlg.h"
+
+class CMainDlg : public CDialogEx {
+public:
+    CMainDlg(CWnd* pParent = nullptr);
+    enum { IDD = IDD_MAIN_DIALOG };
+
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+    virtual BOOL OnInitDialog();
+    void ApplyDarkTheme();
+
+private:
+    CFileBrowserDlg* m_pFileBrowserDlg;
+    CPreferencesDlg* m_pPreferencesDlg;
+    CCommandAliasDlg* m_pCommandAliasDlg;
+
+    afx_msg void OnBnClickedBrowse();
+    afx_msg void OnBnClickedPreferences();
+    afx_msg void OnBnClickedManageCommands();
+
+    DECLARE_MESSAGE_MAP()
+};

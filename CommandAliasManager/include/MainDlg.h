9084c5838422e5e5639fd934ea2e8a14a5dacba8
Will Harrison <willharrison2016@gmail.com>
2024-08-28 09:57:12 -0500

    Update MainDlg.h

--- CommandAliasManager/include/MainDlg.h
+++ CommandAliasManager/include/MainDlg.h
@@ -1,36 +1,29 @@
 #pragma once
 
-#include "afxwin.h"          // For CDialogEx
-#include "FileBrowserDlg.h"  // Include your custom dialog headers
+#include "pch.h"
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/form.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include "FileBrowserDlg.h"
 #include "PreferencesDlg.h"
 #include "CommandAliasDlg.h"
 
-class CMainDlg : public CDialogEx {
+class CMainDlg {
 public:
-    // Constructor for the main dialog
-    CMainDlg(CWnd* pParent = nullptr);  
-    enum { IDD = IDD_MAIN_DIALOG };  // Dialog ID defined in Resource.h
+    CMainDlg();
+    ~CMainDlg();
 
-protected:
-    // MFC override functions for dialog initialization and data exchange
-    virtual void DoDataExchange(CDataExchange* pDX) override;
-    virtual BOOL OnInitDialog() override;
-
-    // Custom function to apply dark theme to the dialog
-    void ApplyDarkTheme();
+    void show();
 
 private:
-    // Pointers to other dialog classes used within this dialog
-    CFileBrowserDlg* m_pFileBrowserDlg;   // File browser dialog
-    CPreferencesDlg* m_pPreferencesDlg;   // Preferences dialog
-    CCommandAliasDlg* m_pCommandAliasDlg; // Command alias dialog
+    void onBrowseClicked();
+    void onPreferencesClicked();
+    void onManageCommandsClicked();
 
-    // Message handlers for button clicks
-    afx_msg void OnBnClickedBrowse();
-    afx_msg void OnBnClickedPreferences();
-    afx_msg void OnBnClickedManageCommands();
-
-    // Macro for declaring the message map in MFC
-    DECLARE_MESSAGE_MAP()
+    nana::form form_;
+    nana::button browse_button_, preferences_button_, manage_commands_button_;
+    CFileBrowserDlg file_browser_dlg_;
+    CPreferencesDlg preferences_dlg_;
+    CCommandAliasDlg command_alias_dlg_;
 };
-

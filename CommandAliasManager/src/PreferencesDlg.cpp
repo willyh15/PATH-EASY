1c71a7784720698d853696ab8f89a600b642cacd
Will Harrison <willharrison2016@gmail.com>
2024-08-28 09:59:19 -0500

    Update PreferencesDlg.cpp

--- CommandAliasManager/src/PreferencesDlg.cpp
+++ CommandAliasManager/src/PreferencesDlg.cpp
@@ -1,52 +1,29 @@
 #include "PreferencesDlg.h"
-#include <afxdlgs.h>
 
-BEGIN_MESSAGE_MAP(PreferencesDlg, CDialogEx)
-    ON_BN_CLICKED(IDC_SAVE_BUTTON, &PreferencesDlg::OnSavePreferences)
-END_MESSAGE_MAP()
+CPreferencesDlg::CPreferencesDlg() {
+    form_.caption("Preferences");
 
-PreferencesDlg::PreferencesDlg(CWnd* pParent /*=nullptr*/)
-    : CDialogEx(IDD_PREFERENCESDIALOG, pParent)
-{
+    preferences_input_.create(form_);
+    save_button_.create(form_);
+    save_button_.caption("Save");
+
+    save_button_.events().click([this] {
+        onSaveClicked();
+    });
+
+    form_.div("<vertical <preferences_input><save_button>>");
+    form_["preferences_input"] << preferences_input_;
+    form_["save_button"] << save_button_;
+    form_.collocate();
 }
 
-void PreferencesDlg::DoDataExchange(CDataExchange* pDX)
-{
-    CDialogEx::DoDataExchange(pDX);
+CPreferencesDlg::~CPreferencesDlg() {}
+
+void CPreferencesDlg::show() {
+    form_.show();
+    nana::exec();
 }
 
-BOOL PreferencesDlg::OnInitDialog()
-{
-    CDialogEx::OnInitDialog();
-    LoadPreferences();
-    return TRUE;
-}
-
-void PreferencesDlg::OnSavePreferences() {
-    // Assume m_recentFilesCountCtrl and m_fileTypeFilterCtrl are input controls
-    CString numRecentFilesStr, fileTypeFilterStr;
-    m_recentFilesCountCtrl.GetWindowText(numRecentFilesStr);
-    m_fileTypeFilterCtrl.GetWindowText(fileTypeFilterStr);
-    
-    int numRecentFiles = _ttoi(numRecentFilesStr);
-    if (numRecentFiles < 0 || numRecentFiles > 100) {
-        AfxMessageBox(_T("Number of recent files must be between 0 and 100."));
-        return;
-    }
-
-    m_numRecentFiles = numRecentFiles;
-    m_defaultFileTypeFilter = fileTypeFilterStr;
-
-    SavePreferences();
-    AfxMessageBox(_T("Preferences saved successfully!"));
-}
-
-void PreferencesDlg::LoadPreferences() {
-    // Load preferences from a config file or registry
-}
-
-
-void PreferencesDlg::LoadPreferences()
-{
-    // Logic to load user preferences
+void CPreferencesDlg::onSaveClicked() {
+    // Save preferences logic here
 }

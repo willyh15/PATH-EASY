cb874846b246d6613453feadd793a667b5dc15a7
Will Harrison <willharrison2016@gmail.com>
2024-08-28 09:52:43 -0500

    Update MainApp.cpp

--- CommandAliasManager/src/MainApp.cpp
+++ CommandAliasManager/src/MainApp.cpp
@@ -1,44 +1,9 @@
+#include <nana/gui.hpp>
 #include "MainDlg.h"
-#include "CommandAliasDlg.h"
-#include "FileBrowserDlg.h"
-#include "PreferencesDlg.h"
 
-BEGIN_MESSAGE_MAP(MainDlg, CDialogEx)
-    ON_BN_CLICKED(IDC_COMMAND_ALIAS_BUTTON, &MainDlg::OnCommandAliasClicked)
-    ON_BN_CLICKED(IDC_FILE_BROWSER_BUTTON, &MainDlg::OnFileBrowserClicked)
-    ON_BN_CLICKED(IDC_PREFERENCES_BUTTON, &MainDlg::OnPreferencesClicked)
-END_MESSAGE_MAP()
-
-MainDlg::MainDlg(CWnd* pParent /*=nullptr*/)
-    : CDialogEx(IDD_MAINDIALOG, pParent)
-{
-}
-
-void MainDlg::DoDataExchange(CDataExchange* pDX)
-{
-    CDialogEx::DoDataExchange(pDX);
-}
-
-BOOL MainDlg::OnInitDialog()
-{
-    CDialogEx::OnInitDialog();
-    return TRUE;
-}
-
-void MainDlg::OnCommandAliasClicked()
-{
-    CommandAliasDlg commandAliasDlg;
-    commandAliasDlg.DoModal();
-}
-
-void MainDlg::OnFileBrowserClicked()
-{
-    FileBrowserDlg fileBrowserDlg;
-    fileBrowserDlg.DoModal();
-}
-
-void MainDlg::OnPreferencesClicked()
-{
-    PreferencesDlg preferencesDlg;
-    preferencesDlg.DoModal();
+int main() {
+    CMainDlg mainDialog;
+    mainDialog.show();
+    nana::exec(); // Start the Nana event loop
+    return 0;
 }

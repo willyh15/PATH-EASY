dadf17c749e29ccb7e21a2233b028b6744c1229b
Will Harrison <willharrison2016@gmail.com>
2024-08-29 09:48:56 -0500

    Update AliasManager.cpp

--- CommandAliasManager/src/AliasManager.cpp
+++ CommandAliasManager/src/AliasManager.cpp
@@ -11,7 +11,17 @@ Json::Value AliasManager::LoadAliases() {
     std::ifstream file(alias_file_path, std::ifstream::binary);
     Json::Value aliases;
     if (file) {
-        file >> aliases;
+        try {
+            file >> aliases;
+        } catch (const std::exception& e) {
+            nana::msgbox m("Error");
+            m << "Failed to load aliases: " << e.what();
+            m.show();
+        }
+    } else {
+        nana::msgbox m("Error");
+        m << "Unable to open alias file for reading.";
+        m.show();
     }
     return aliases;
 }
@@ -20,10 +30,16 @@ Json::Value AliasManager::LoadAliases() {
 void AliasManager::SaveAliases(const Json::Value& aliases) {
     std::ofstream file(alias_file_path, std::ofstream::binary);
     if (file) {
-        file << aliases;
+        try {
+            file << aliases;
+        } catch (const std::exception& e) {
+            nana::msgbox m("Error");
+            m << "Failed to save aliases: " << e.what();
+            m.show();
+        }
     } else {
         nana::msgbox m("Error");
-        m << "Unable to save aliases.";
+        m << "Unable to open alias file for writing.";
         m.show();
     }
 }

30efdd3d896d0d5f0373a0e4b9f520e5b737bf42
Will Harrison <willharrison2016@gmail.com>
2024-10-01 14:35:23 -0500

    Delete CommandAliasManager/main.cpp

--- CommandAliasManager/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "MainDlg.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  CMainDlg mainDialog;
-  mainDialog.show();
-  return app.exec();
-}
